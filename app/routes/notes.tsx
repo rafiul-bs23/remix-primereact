a957eda1a9c19b755f4c6ce74cdb7113ae94394f
Rafiul Islam <rafiul.islam@brainstation-23.com>
2024-07-03 20:19:48 +0600

    store data in json file

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,8 +1,20 @@
+import { redirect } from "@remix-run/node"
 import NewNote from "~/components/NewNote"
+import { getStoredNotes, storeNotes } from "~/data/notes"
 export default function NotesPage() {
-    return (
-        <main>
-            <NewNote/>
-        </main>
-    )
+  return (
+    <main>
+      <NewNote />
+    </main>
+  )
+}
+export async function action({ request }: { request: Request }) {
+  const formData = await request.formData()
+  const noteData = Object.fromEntries(formData)
+
+  const existingNotes = await getStoredNotes()
+  noteData.id = new Date().toISOString()
+  const updatedNotes = existingNotes.concat(noteData)
+  await storeNotes(updatedNotes)
+  return redirect("/notes")
 }
